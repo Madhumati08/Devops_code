2f16004ec22dd9ea7f96954dc910e8e403effe83
Madhumati08 <madhumatisavoor08@gmai.com>
2026-03-20 17:09:06 +0530

    remove files

--- Main.tf
+++ Main.tf
@@ -2,8 +2,3 @@ resource "aws_instance" "example" {
   ami           = "ami-0c55b159cbfafe1d0"
   instance_type = "t2.micro"
 }
-resource "aws_accessanalyzer_analyzer" "name" {
-  
-    analyzer_name = "example-analyzer"
-    type          = "ACCOUNT"
-    }
