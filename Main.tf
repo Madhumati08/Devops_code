960a4b67f86a2e05d18aaed84819a643b2425a97
Madhumati08 <madhumatisavoor08@gmai.com>
2026-03-20 16:45:35 +0530

    commited files

--- Main.tf
+++ Main.tf
@@ -2,3 +2,8 @@ resource "aws_instance" "example" {
   ami           = "ami-0c55b159cbfafe1d0"
   instance_type = "t2.micro"
 }
+resource "aws_accessanalyzer_analyzer" "name" {
+  
+    analyzer_name = "example-analyzer"
+    type          = "ACCOUNT"
+    }
